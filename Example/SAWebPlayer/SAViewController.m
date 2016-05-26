8acfa86eb13f41ce5ef5509e09ef328f664993fe
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-05-26 12:27:01 +0100

    update

---
+++ Example/SAWebPlayer/SAViewController.m
@@ -0,0 +1,29 @@
+//
+//  SAViewController.m
+//  SAWebPlayer
+//
+//  Created by Gabriel Coman on 03/08/2016.
+//  Copyright (c) 2016 Gabriel Coman. All rights reserved.
+//
+
+#import "SAViewController.h"
+
+@interface SAViewController ()
+
+@end
+
+@implementation SAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
