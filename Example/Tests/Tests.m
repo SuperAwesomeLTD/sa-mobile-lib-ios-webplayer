67e7f422f626c43aa6e1ee2b2d5f5f67bc089156
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-05-26 12:43:53 +0100

    updated tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -1,12 +1,12 @@
 //
-//  SAWebPlayerTests.m
-//  SAWebPlayerTests
+//  Tests.m
+//  Tests
 //
-//  Created by Gabriel Coman on 03/08/2016.
-//  Copyright (c) 2016 Gabriel Coman. All rights reserved.
+//  Created by Gabriel Coman on 26/05/2016.
+//  Copyright © 2016 Gabriel Coman. All rights reserved.
 //
 
-@import XCTest;
+#import <XCTest/XCTest.h>
 
 @interface Tests : XCTestCase
 
@@ -14,20 +14,26 @@
 
 @implementation Tests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
+- (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
 }
 
 @end
-
